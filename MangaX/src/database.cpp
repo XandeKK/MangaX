42150fa119d627c77ec73932b0ad85ca3324e182
Alexandre <alexandre.dosa@gmail.com>
2022-03-05 12:53:04 -0300

    Update database.cpp

--- MangaX/src/database.cpp
+++ MangaX/src/database.cpp
@@ -5,3 +5,41 @@ Database::Database(QObject *parent)
 {
 
 }
+
+void Database::openDatabase() {
+    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName("database.db");
+
+    if(!m_db.open()){
+        qDebug() << "Error: connection with database failed";
+    }else {
+        qDebug() << "Database: connection ok";
+    }
+}
+
+void Database::createTable()
+{
+    query.exec("CREATE TABLE IF NOT EXISTS manga(name VARCHAR(255) NOT NULL UNIQUE, url VARCHAR(255) NOT NULL, chapter INT not null");
+}
+
+void Database::addManga(QString &name, QString &url, QString &chapter)
+{
+    query.prepare("INSERT INTO manga VALUES (:name, :url, :chapter)");
+    query.bindValue(":name", name);
+    query.bindValue(":url", url);
+    query.bindValue(":chapter", chapter);
+    query.exec();
+}
+
+void Database::removeManga(QString &name)
+{
+    query.prepare("DELETE FROM manga WHERE name = :name");
+    query.bindValue(":name", name);
+    query.exec();
+}
+
+void Database::editManga(QString &nameOld, QString &name, QString &url, QString &chapter)
+{
+    removeManga(nameOld);
+    addManga(name, url, chapter);
+}
