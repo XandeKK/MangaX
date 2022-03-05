f741ff9f6057a8c6475466596e93517fbbb41fa1
Alexandre <alexandre.dosa@gmail.com>
2022-03-05 12:53:08 -0300

    Update database.h

--- MangaX/src/database.h
+++ MangaX/src/database.h
@@ -2,6 +2,9 @@
 #define DATABASE_H
 
 #include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
 
 class Database : public QObject
 {
@@ -9,10 +12,17 @@ class Database : public QObject
 public:
     explicit Database(QObject *parent = nullptr);
 
-    void addManga();
-    void removeManga();
-    void editManga();
+    void openDatabase();
+    void createTable();
 
+    Q_INVOKABLE void addManga(QString &name, QString &url, QString &chapter);
+    Q_INVOKABLE void removeManga(QString &name);
+    Q_INVOKABLE void editManga(QString &nameOld, QString &name, QString &url, QString &chapter);
+
+
+private:
+    QSqlDatabase m_db;
+    QSqlQuery query;
 
 signals:
 
