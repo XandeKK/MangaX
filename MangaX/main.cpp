c08f13a0f0d493d5e2d4b16d4c4f2d914528ea8a
Alexandre <alexandre.dosa@gmail.com>
2022-03-05 12:54:20 -0300

    Update main.cpp

--- MangaX/main.cpp
+++ MangaX/main.cpp
@@ -1,13 +1,20 @@
 #include <Python.h>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <qqml.h>
+#include <qqmlcontext.h>
+
+#include "src/database.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    Database db;
+
     QQmlApplicationEngine engine;
     const QUrl url("qrc:/qml/main.qml");
+    engine.rootContext()->setContextProperty( "_database", &db );
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
